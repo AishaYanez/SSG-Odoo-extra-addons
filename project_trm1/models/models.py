a168f5b76adadacf1f27ed78f38932e3a9f50412
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-12-14 02:37:24 +0000

    Add menuItem task

--- project_trm1/models/models.py
+++ project_trm1/models/models.py
@@ -11,6 +11,9 @@ class project_trm1_contrating_companies(models.Model):
  employees = fields.Integer(string="Número de empleados")
  logo= fields.Binary(string="Logo de la empresa")
  company_size = fields.Char(string="Tamaño de la empresa", compute="_companysize", store=True)
+#  digital_signature = fields.Binary(string="Firma",related='user_id.digital_signature',readonly=True)
+
+#  user_id = fields.Many2one('res.user',string='Usuario que le da de alta')
 
  @api.depends('employees')
  def _companysize(self):
@@ -25,6 +28,7 @@ class project_trm1_project(models.Model):
  _inherit = 'project.project'
  
  company = fields.Many2one("project_trm1.contrating_companies",string="Empresa",required=True,ondelete="cascade")
+#  user_id = fields.Many2one('res.user',string='Jefe de proyecto')
 
 class project_trm1_task(models.Model):
  _name = 'project.task'
